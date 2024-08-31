bf671720b9e004c0338df4a7ec578ceb378ee703
kunaal-v <kunalfzk2001@gmail.com>
2024-08-31 19:43:16 +0530

    make html structure of project

---
+++ index.js
@@ -0,0 +1,6 @@
+const search_btn=document.querySelector(".search_btn");
+const current_location_btn=document.querySelector(".current_location_btn");
+console.log(search_btn);
+console.log(current_location_btn);
+const temp=fetch("https://open-meteo.com/");
+console.log(temp);
